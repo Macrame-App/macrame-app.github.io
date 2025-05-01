a4720fb7bb3eb734bfc953a256aae472a155acb0
Jesse Malotaux <jemx@mboutrecht.nl>
2025-05-01 20:33:12 +0200

    config update

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   description: "Documentation for the macro button panel app called Macrame.",
   themeConfig: {
     siteTitle: "Macrame",
-    logo: "/docs/assets/img/Macrame-Logo-gradient.svg",
+    logo: "assets/img/Macrame-Logo-gradient.svg",
     footer: {
       message: "Licensed under the GNU GPLv3 <a href='license'>license</a>",
       copyright: "Copyright © 2025-present <a href='https://github.com/JaxxMoss'>Jesse Malotaux</a>",
@@ -17,41 +17,41 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "Home", link: "/" },
-      { text: "Installation", link: "/docs/getting-started/installation" },
+      { text: "Installation", link: "getting-started/installation" },
     ],
 
     sidebar: [
       {
         text: "Getting Started",
         items: [
-          { text: "Installation", link: "/docs/getting-started/installation" },
-          { text: "What is Macrame?", link: "/docs/getting-started/what-is" },
+          { text: "Installation", link: "getting-started/installation" },
+          { text: "What is Macrame?", link: "getting-started/what-is" },
         ],
       },
       {
         text: "Devices",
-        items: [{ text: "Linking devices", link: "/docs/devices/linking" }],
+        items: [{ text: "Linking devices", link: "devices/linking" }],
       },
       {
         text: "Panels",
         items: [
-          { text: "Usage", link: "/docs/panels/usage" },
-          { text: "Linking macros", link: "/docs/panels/linking-macros" },
-          { text: "Creation", link: "/docs/panels/creation" },
+          { text: "Usage", link: "panels/usage" },
+          { text: "Linking macros", link: "panels/linking-macros" },
+          { text: "Creation", link: "panels/creation" },
         ],
       },
       {
         text: "Macros",
         items: [
-          { text: "Usage", link: "/docs/macros/usage" },
-          { text: "Recording", link: "/docs/macros/recording" },
-          { text: "Editing", link: "/docs/macros/editing" },
+          { text: "Usage", link: "macros/usage" },
+          { text: "Recording", link: "macros/recording" },
+          { text: "Editing", link: "macros/editing" },
         ],
       },
       {
         text: "About",
         items: [
-          { text: "Creator", link: "/docs/about/creator" },
+          { text: "Creator", link: "about/creator" },
           { text: "License", link: "/license" },
         ],
       },
