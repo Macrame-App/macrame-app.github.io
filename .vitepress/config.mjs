7fb653705f50d12e30a83408c0e55e71afc723d2
Jesse Malotaux <jemx@mboutrecht.nl>
2025-05-01 20:02:01 +0200

    Exclude README.md from vitepress

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: "/",
-  exclude: ["README.md"],
+  srcExclude: ['**/README.md'],
   title: "Macrame - Documentation",
   description: "Documentation for the macro button panel app called Macrame.",
   themeConfig: {
